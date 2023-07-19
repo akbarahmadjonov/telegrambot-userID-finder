78e3dc23d041dcfb23c13b8c2bd15baf6ed2612c
akbarahmadjonov <akhmadjonovakbardeveloper@gmail.com>
2023-07-19 12:48:09 +0500

    Add encoder

--- src/app.js
+++ src/app.js
@@ -2,13 +2,10 @@ require("dotenv/config");
 const { Bot, InlineKeyboard } = require("grammy");
 const Io = require("./utils/Io");
 const Users = new Io("./db/users.json");
-const { TextEncoder } = require("text-encoding");
+var util = require("util");
+var encoder = new util.TextEncoder("utf-8");
 
 const bot = new Bot(process.env.TOKEN);
-
-const text = "Hello, world!";
-const encoder = new TextEncoder();
-const encodedText = encoder.encode(text);
 
 //* Shows developers
 bot.command("behindscene", async (ctx) => {
